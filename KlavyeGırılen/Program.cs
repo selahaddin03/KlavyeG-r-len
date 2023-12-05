3e6caf858c7cc5002c08bb0b24f721a8167923fd
selahaddin03 <149465780+selahaddin03@users.noreply.github.com>
2023-12-05 19:37:53 +0300

    Proje dosyası ekle.

---
+++ KlavyeGırılen/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlavyeGırılen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KlavyeGırılenSayi klavye = new KlavyeGırılenSayi();
+            klavye.toplaMetodu();
+            
+
+
+
+
+
+
+
+
+        }
+    }
+        
+}
+public class KlavyeGırılenSayi
+{
+    public void toplaMetodu()
+    {
+
+        int sayi, toplam = 0;
+        while (true)
+        {
+            Console.Write("Sayı Gir : ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            if (sayi == 0)
+            {
+                break;
+            }
+            toplam += sayi;
+        }
+        Console.WriteLine("Girdiğiniz Sayıların Toplamı: " + toplam);
+        Console.ReadKey();
+    }
+}   
+
+
+
